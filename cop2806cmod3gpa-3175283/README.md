ab430938a06aec619a5aa7945a8836e4ef82be16
wgeesey <89809942+wgeesey@users.noreply.github.com>
2023-06-27 15:19:20 -0400

    Add files via upload

---
+++ cop2806cmod3gpa-3175283/README.md
@@ -0,0 +1,3 @@
+# COP2806C Module 3 Programming Assignment
+
+## Summer 2023
---
+++ cop2806cmod3gpa-3175283/isf6/chapter03/src/main/java/chapter03/model/DocumentType.java
@@ -0,0 +1,20 @@
+package chapter03.model;
+
+public enum DocumentType {
+  PDF("PDF", "Portable Document Format", ".pdf"),
+  DOCX("DOCX", "Word Document", ".docx"),
+  URL("URL", "Universal Resource Locator", ""),
+  DOC("DOC", "Word Document", ".doc"),
+  NOTE("NOTE", "Ancillary note", ""),
+  TXT("TXT", "Text File", ".txt");
+
+  public final String name;
+  public final String desc;
+  public final String extension;
+
+  private DocumentType(String name, String desc, String extension) {
+    this.name=name;
+    this.desc=desc;
+    this.extension=extension;
+  }
+}
