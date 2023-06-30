11ecaa0c0cabf76e68cfa66aea4490f7c19aa8a2
wgeesey <89809942+wgeesey@users.noreply.github.com>
2023-06-30 11:00:36 -0400

    Add files via upload

---
+++ cop2806cmod6gpa-3175283/README.md
@@ -0,0 +1,3 @@
+# COP2806C Module 6 Programming Assignment
+
+## Summer 2023
---
+++ cop2806cmod6gpa-3175283/isf6/chapter06/src/main/java/c06/SearchEngine.java
@@ -0,0 +1,11 @@
+package c06;
+
+import java.util.List;
+
+public interface SearchEngine {
+  List<Document> findByType(DocumentType documentType);
+
+  List<Document> listAll();
+
+
+}
